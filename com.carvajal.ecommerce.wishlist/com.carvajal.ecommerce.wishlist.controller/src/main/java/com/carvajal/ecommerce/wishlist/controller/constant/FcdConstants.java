3da65d5a9ef6a7eb4a5e08a4e022329a7d466ae7
andresfbastidas <andresfbastidas@hotmail.com>
2021-11-30 01:30:48 -0500

    add file constants and commons

---
+++ com.carvajal.ecommerce.wishlist/com.carvajal.ecommerce.wishlist.controller/src/main/java/com/carvajal/ecommerce/wishlist/controller/constant/FcdConstants.java
@@ -0,0 +1,15 @@
+package com.carvajal.ecommerce.wishlist.controller.constant;
+
+public class FcdConstants {
+
+	private static final String VERSION_API = "1.0";
+
+	public static final String CONTEXT_API = "/api/" + VERSION_API;
+
+	public static final String USER = CONTEXT_API + "/user";
+
+	public static final String LOGIN = "/authenticate";
+
+	public static final String CREATE_USER = "/createUser";
+
+}
---
+++ com.carvajal.ecommerce.wishlist/com.carvajal.ecommerce.wishlist.model/src/main/java/com/carvajal/ecommerce/wishlist/model/constant/KeyConstants.java
@@ -0,0 +1,7 @@
+package com.carvajal.ecommerce.wishlist.model.constant;
+
+public class KeyConstants {
+
+	public static final String SUCCESS_CREATE_USER = "Usuario creado con exito";
+
+}
